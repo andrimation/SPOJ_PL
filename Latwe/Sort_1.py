ed434cf9064286b9cdcbe5094e99ed1b1c43d1b8
andrimation <andrimation@gmail.com>
2021-08-17 22:45:22 +0200

    exercises

--- Latwe/Sort_1.py
+++ Latwe/Sort_1.py
@@ -1,76 +1,44 @@
 import math, sys
 
-dataInput = []
-lineCounter = 0
-for line in sys.stdin:
-    dataInput.append((line.strip()).split())
-
-
-    lineCounter +=1
-print(dataInput)
-
-
-
-
-
-
-
 mainData = []
 
-for test in range(testCounter):
-    listofData = []
-    numberOfPoints = int(input())
-    for point in range(numberOfPoints):
-        alpha,x,y = input().split()
-        listofData.append([alpha,int(x),int(y)])
-    print()
-    mainData.append(listofData)
+testCounter = int(sys.stdin.readline())
 
-# print(mainData,"main data")
+# Przyjęcie danych
+for x in range(2):
+    listOfData = []
+    numberOfPoints = sys.stdin.readline()
+    # print(int(numberOfPoints))
+    for q in range(int(numberOfPoints)):
+        alpha,x,y = (sys.stdin.readline()).split()
+        listOfData.append([alpha,int(x),int(y)])
+    enterPlaceholder = sys.stdin.readline()
+    mainData.append(listOfData)
 
 
 result = []
 
-
+# Sortowanie
 for smallList in mainData:
     sortedList = []
-    # print(smallList, "small list")
     listofDataCopy = smallList.copy()
     while listofDataCopy:
         maxDistance = listofDataCopy[0]
-        # print(maxDistance)
         for bracket in listofDataCopy:
-            # print(listofDataCopy)
-            # print(bracket)
-            # print(maxDistance)
-            # print(maxDistance[1],maxDistance[2],bracket[1],bracket[2])
-            if (math.fabs(maxDistance[1]) + math.fabs(maxDistance[2])) < (math.fabs(bracket[1])+math.fabs(bracket[2])):
+            if math.sqrt(((math.fabs(maxDistance[1]))**2 + (math.fabs(maxDistance[2])))**2) < math.sqrt((math.fabs(bracket[1]))**2+(math.fabs(bracket[2]))**2):
                 maxDistance = bracket
 
         sortedList.append(maxDistance)
         listofDataCopy.remove(maxDistance)
 
-
-
     result.append(sortedList)
-# print(result,"Result")
 
-
+# Wyświetlenie danych
 for dataSet in result:
     dataSet.reverse()
-    for bracket in range(len(dataSet)):
-        for x in range(3):
-            if x != 2:
-                print(dataSet[bracket][x],end=" ")
-            else:
-                print(dataSet[bracket][x],end="")
-        if bracket != len(dataSet):
-            print()
-        else:
-            pass
+    for bracket in dataSet:
+        print(bracket[0]," ",bracket[1]," ",bracket[2])
     print()
-
-# print(result)
 
 
 
