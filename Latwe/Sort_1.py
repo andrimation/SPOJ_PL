eb8cc99822308784a8915cbd48383366d5dfcdb9
andrimation <andrimation@gmail.com>
2021-08-18 19:46:52 +0200

    Sort_1

--- Latwe/Sort_1.py
+++ Latwe/Sort_1.py
@@ -5,16 +5,15 @@ mainData = []
 testCounter = int(sys.stdin.readline())
 
 # Przyjęcie danych
-for x in range(2):
+for x in range(testCounter):
     listOfData = []
     numberOfPoints = sys.stdin.readline()
-    # print(int(numberOfPoints))
     for q in range(int(numberOfPoints)):
-        alpha,x,y = (sys.stdin.readline()).split()
-        listOfData.append([alpha,int(x),int(y)])
-    enterPlaceholder = sys.stdin.readline()
+        alpha, x, y = (sys.stdin.readline()).split()
+        listOfData.append([alpha, int(x), int(y)])
+    if testCounter != 1:
+        enterPlaceholder = sys.stdin.readline()
     mainData.append(listOfData)
-
 
 result = []
 
@@ -25,9 +24,9 @@ for smallList in mainData:
     while listofDataCopy:
         maxDistance = listofDataCopy[0]
         for bracket in listofDataCopy:
-            if math.sqrt(((math.fabs(maxDistance[1]))**2 + (math.fabs(maxDistance[2])))**2) < math.sqrt((math.fabs(bracket[1]))**2+(math.fabs(bracket[2]))**2):
+            if (maxDistance[1]**2 + maxDistance[2]**2)**0.5 > math.sqrt(
+                    (math.fabs(bracket[1])) ** 2 + (math.fabs(bracket[2])) ** 2):
                 maxDistance = bracket
-
         sortedList.append(maxDistance)
         listofDataCopy.remove(maxDistance)
 
@@ -35,12 +34,10 @@ for smallList in mainData:
 
 # Wyświetlenie danych
 for dataSet in result:
-    dataSet.reverse()
     for bracket in dataSet:
-        print(bracket[0]," ",bracket[1]," ",bracket[2])
-    print()
-
-
+        print(bracket[0], bracket[1], bracket[2])
+    if bracket != dataSet[-1][-1]:
+        print()
 
 
 
