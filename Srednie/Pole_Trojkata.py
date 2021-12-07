50e73a4226e1989c9c49232c5fff3a7b46c75056
andrimation <88882438+andrimation@users.noreply.github.com>
2021-12-07 12:37:11 +0100

    Update Pole_Trojkata.py
    AC

--- Srednie/Pole_Trojkata.py
+++ Srednie/Pole_Trojkata.py
@@ -1,4 +1,4 @@
-import sys, math, numpy
+import sys, math
 
 
 def triangleField(pointA, pointB, pointC):
@@ -13,45 +13,80 @@ for test in range(testCount):
     pointsListX = []
     pointsCount = int(data.readline())
 
-    for line in range(1, pointsCount + 1):
-        point = list(map(int, data.readline().split()))
-        point.append(line)
-        pointsListX.append(list(point))
+    pointsListX = [list(map(int, (data.readline().split() + [x]))) for x in range(1, pointsCount + 1)]
+
+
+
+    pointsListX.sort(key=lambda x: x[0])
+
+    pointsListY = pointsListX.copy()
+    pointsListY.sort(key=lambda x: x[1])
+    pointsListYrev = pointsListY.copy()
+    pointsListYrev.reverse()
+
     trianglesCount = int(pointsCount / 3)
-    pointStatus = numpy.full(len(pointsListX), False, dtype=object)
+    pointStatus = [False for x in range(pointsCount)]
 
-    for triangle in range(trianglesCount):
+    xMaxIteration = 0
+    xMinIteration = 0
 
-        pointsListX.sort(key=lambda x: x[0])
-        xMax = pointsListX[0]
-        pointsListX.remove(xMax)
-        xMin = pointsListX[-1]
-        pointsListX.remove(xMin)
+    yMaxIteration = 0
+    yMinIteration = 0
 
-        pointsListX.sort(key=lambda x: x[1])
-        yMax = pointsListX[0]
+    for x in range(trianglesCount):
+        xMax = pointsListX[xMaxIteration]
+        if pointStatus[xMax[2] - 1] == True:
+            while True:
+                xMaxIteration += 1
+                xMax = pointsListX[xMaxIteration]
 
-        candidates = [xMax, xMin, yMax]
-        try:
-            yMin = pointsListX[-1]
-            candidates.append(yMin)
-        except:
-            pass
+                if pointStatus[xMax[2] - 1] == False:
+                    break
+        pointStatus[xMax[2] - 1] = True
 
-        if len(candidates) == 4:
-            maxField = 0
-            for pointC in candidates:
-                if pointStatus[pointC[2] - 1] != True:
-                    field = triangleField(xMax, xMin, pointC)
-                    if field > maxField:
-                        maxField = field
-                        answer3 = pointC
-        else:
-            answer3 = candidates[2]
+        xMin = pointsListX[((-1) * xMinIteration) - 1]
+        if pointStatus[xMin[2] - 1] == True:
+            while True:
+                xMinIteration += 1
+                xMin = pointsListX[((-1) * xMinIteration) - 1]
+                if pointStatus[xMin[2] - 1] == False:
+                    break
+
+        pointStatus[xMin[2] - 1] = True
+
+        yMaxIterationSave = yMaxIteration
+
+        yMax = pointsListY[yMaxIteration]
+        if pointStatus[yMax[2] - 1] == True:
+            while True:
+                yMaxIteration += 1
+                yMax = pointsListY[yMaxIteration]
+                if pointStatus[yMax[2] - 1] == False:
+                    break
+
+        yMinIterationSave = yMinIteration
+
+        yMin = pointsListYrev[yMinIteration]
+        if pointStatus[yMin[2] - 1] == True:
+            while True:
+                yMinIteration += 1
+                yMin = pointsListYrev[yMinIteration]
+                if pointStatus[yMin[2] - 1] == False:
+                    break
+
+        candidates = [yMax, yMin]
+
+        maxField = 0
+        for pointC in candidates:
+            field = triangleField(xMax, xMin, pointC)
+            if field > maxField:
+                maxField = field
+                answer3 = pointC
 
         myNumbers = [xMax[2], xMin[2], answer3[2]]
         myNumbers.sort()
         print(myNumbers[0], myNumbers[1], myNumbers[2])
-        pointsListX.remove(answer3)
+
         pointStatus[answer3[2] - 1] = True
+
     print()
